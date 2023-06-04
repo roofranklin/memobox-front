e909773a39033efbc94c5d3eb9e92a27a2fcdd23
Roosevelt Franklin <roosevelt_franklin@hotmail.com>
2023-06-04 11:12:24 -0300

    Instalação React + Vite +  Styled Components

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import styled from 'styled-components'
+
+const Button = styled.button`
+  background-color: blue;
+  color: white;
+`
+
+function App() {
+
+  return (
+    <>
+     <h1>Memobox</h1>
+     <Button>Clique aqui</Button>
+    </>
+  )
+}
+
+export default App
