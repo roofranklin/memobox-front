dbe2a7091df612c5c79a8a7f3f653c50aeceec16
Roosevelt Franklin <roosevelt_franklin@hotmail.com>
2023-06-04 22:34:19 -0300

    Carousel funcionando

--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -1,13 +1,32 @@
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+
+import AliceCarousel from 'react-alice-carousel';
+import 'react-alice-carousel/lib/alice-carousel.css';
 import { LoginContainer,
          Sidebar,
          SidebarContainer,
          SidebarTitle,
+         Testimonial,
          LoginContent } from './login.styles'
 import Logo from '../../components/logo-branca'
 
+const handleDragStart = (e) => e.preventDefault();
+
+const testimonials = [
+    <Testimonial key="01" onDragStart={handleDragStart} role="presentation"></Testimonial>,
+    <Testimonial key="02" onDragStart={handleDragStart} role="presentation"></Testimonial>,
+    <Testimonial key="03" onDragStart={handleDragStart} role="presentation"></Testimonial>,
+];
+
+const responsive = {
+    0: { items: 1 },
+    568: { items: 1 },
+    1024: { items: 1.5 },
+};
+
 const Login = () => {
+
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const navigate = useNavigate();
@@ -36,6 +55,15 @@ const Login = () => {
             <SidebarContainer>
                 <Logo />
                 <SidebarTitle>Faça como dezenas de estudantes_</SidebarTitle>
+                <AliceCarousel
+                    mouseTracking
+                    items={testimonials}
+                    responsive={responsive}
+                    autoPlayStrategy="default"
+                    animationType="slide"
+                    animationDuration={1000}
+                    disableButtonsControls
+                />
             </SidebarContainer>
         </Sidebar>
         <LoginContent>
